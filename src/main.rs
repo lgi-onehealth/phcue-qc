cd9c6b8a6ece2ae3c55f588caad9685bb259f3bb
Anders Goncalves da Silva <andersgs@gmail.com>
2021-12-16 14:25:18 -0800

    Implement FastqStats with Arc and Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate bloom;
 extern crate fastq;
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+// use std::sync::atomic::{AtomicU32, AtomicU64};
 // use std::collections::HashMap;
 use std::time::{Instant};
 use structopt::StructOpt;
@@ -33,40 +35,87 @@ struct Opt {
     file_name: Option<String>,
 }
 
+#[derive(Debug, Copy, Clone)]
+struct FastqStats {
+    num_reads: u32,
+    num_bases: u64,
+    min_read_len: u16,
+    max_read_len: u16,
+    total_q_score: u64,
+}
+
+impl FastqStats {
+    fn new() -> Self {
+        FastqStats {
+            num_reads: 0,
+            num_bases: 0,
+            min_read_len: std::u16::MAX,
+            max_read_len: 0,
+            total_q_score: 0,
+        }
+    }
+
+    fn update(&mut self, local_num_reads: u32, local_num_bases: u64, 
+        local_min_read_len: u16, local_max_read_len: u16, local_total_q_score: u64) {
+        self.num_reads += local_num_reads;
+        self.num_bases += local_num_bases;
+        self.min_read_len = std::cmp::min(self.min_read_len, local_min_read_len);
+        self.max_read_len = std::cmp::max(self.max_read_len, local_max_read_len);
+        self.total_q_score += local_total_q_score;
+    }
+
+    fn print_stats(self, offset: Option<f64>) {
+        println!("Total reads: {}", self.num_reads);
+        println!("Total bases: {}", self.num_bases);
+        println!("Min read len: {}", self.min_read_len);
+        println!("Max read len: {}", self.max_read_len);
+        println!("Mean Q score: {}", self.mean_q_score(offset));
+    }
+
+    fn _get(self) -> (u32, u64, u16, u16, u64, u64) {
+        (self.num_reads, self.num_bases, self.min_read_len, self.max_read_len, self.total_q_score, self.total_q_score)
+    }
+
+    fn mean_q_score(self, offset: Option<f64>) -> f64 {
+        (self.total_q_score as f64 / self.num_bases as f64) - offset.unwrap_or(33.0)
+    }    
+}
+
 fn main() {
     let opt = Opt::from_args();
     eprintln!("Working on: {:?}", opt.input);
+    let fastq_stats = Arc::new(Mutex::new(FastqStats::new()));
     // let mut bf: BloomFilter = BloomFilter::with_rate(0.01, 10_000_000);
     // let mut kmer_counter: HashMap<String, u64> = HashMap::with_capacity(10_000_000);
     eprintln!("Starting to parse file...");
     let start = Instant::now();
     parse_path(Some(&opt.input), |parser| {
         let n_threads = 4;
-        let results: Vec<(u32,u64,u64)> = parser.parallel_each(n_threads, move |record_sets| {
+        let local_stats = Arc::clone(&fastq_stats);
+        let _results: Vec<u32> = parser.parallel_each(n_threads, move |record_sets| {
             let mut total_reads: u32 = 0;
             let mut total_bases: u64 = 0;
             let mut total_qual: u64 = 0;
+            let mut min_read_len: u16 = std::u16::MAX;
+            let mut max_read_len: u16 = 0;
             for record_set in record_sets {
                 for _record in record_set.iter() {
                     total_reads += 1;
                     total_bases += _record.seq().len() as u64;
                     total_qual += _record.qual().iter().map(|x| *x as u64).sum::<u64>();
-        }
+                    min_read_len = std::cmp::min(min_read_len, _record.seq().len() as u16);
+                    max_read_len = std::cmp::max(max_read_len, _record.seq().len() as u16);
+                }
             }
-            (total_reads, total_bases, total_qual)
+            let mut ls = local_stats.lock().unwrap();
+            ls.update(total_reads, total_bases, min_read_len, max_read_len, total_qual);
+            total_reads
         }).expect("Invalid FASTQ file.");
         let qual_offset = 33.0;
         let elapsed = start.elapsed().as_secs() as f64;
-        let total_reads: u32 = results.iter().map(|x| x.0).sum();
-        let total_bases: u64 = results.iter().map(|x| x.1).sum();
-        let mean_q_score: f64 = (results.iter().map(|x| x.2).sum::<u64>() as f64 / total_bases as f64) - qual_offset;
-        let reads_per_second = total_reads as f64 / elapsed;
-        println!("Total reads: {} ({} reads/second)", total_reads, reads_per_second);
-        println!("Total bases: {}", total_bases);
-        println!("Total bases (parallel): {}", par_total_nucleotides.load(std::sync::atomic::Ordering::Relaxed));
-        println!("Mean quality score: {}", mean_q_score);
-        println!("Total time spent: {}s", elapsed);
-        println!("Total readsets processed: {}", results.len());
-        println!("The results vector: {:?}", results);
+        let fq_lock = fastq_stats.lock().unwrap();
+        fq_lock.print_stats(Some(qual_offset));
+        println!("Elapsed time: {} seconds", elapsed);
+        println!("Processing rate: {} read/second", fq_lock.num_reads as f64 / elapsed);
     }).expect("Invalid compression");
 }
